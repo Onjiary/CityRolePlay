a70658599caf612a033159b74d32d0f875213de6
Onjiary <onjiary@gmail.com>
2021-01-23 23:18:35 +0100

    resource

--- cityroleplay-client/client/playerblips.js
+++ cityroleplay-client/client/playerblips.js
@@ -13,7 +13,6 @@ LPSDBLIP.color = 3;
 LPSDBLIP.name = "LSPD";
 LPSDBLIP.shortRange = 1;
 
-
 const flughafen = new alt.PointBlip(-1042.4967, -2745.8901, 21.343628);
 flughafen.sprite = 572;
 flughafen.color = 40;
@@ -52,10 +51,119 @@ Autohaus.name = "Premium Deluxe Motorsport";
 Autohaus.shortRange = 1;
 
 // 24/7 Läden:
-// -51.204395, -1754.6505, 29.414673
+const Einkkaufsladen = new alt.PointBlip(-51.204395, -1754.6505, 29.414673);
+Einkkaufsladen.sprite = 59;
+Einkkaufsladen.color = 43;
+Einkkaufsladen.name = "24/7";
+Einkkaufsladen.shortRange = 1;
 
-// 28.905497, -1347.0461, 29.482056
+const Einkkaufsladen2 = new alt.PointBlip(28.905497, -1347.0461, 29.482056);
+Einkkaufsladen2.sprite = 59;
+Einkkaufsladen2.color = 43;
+Einkkaufsladen2.name = "24/7";
+Einkkaufsladen2.shortRange = 1;
 
-//
+// Kleiderläden:
+const Kleiderladen = new alt.PointBlip(125.063736, -216.77802,  54.554565);
+Kleiderladen.sprite = 73;
+Kleiderladen.color = 47;
+Kleiderladen.name = "Kleidungsgeschäft";
+Kleiderladen.shortRange = 1;
 
-//
+const Kleiderladen2 = new alt.PointBlip(-819.53406,  -1075.6615,  11.317993);
+Kleiderladen2.sprite = 73;
+Kleiderladen2.color = 47;
+Kleiderladen2.name = "Kleidungsgeschäft";
+Kleiderladen2.shortRange = 1;
+
+const WeazelNews = new alt.PointBlip(-599.61755, -930.3956, 23.854248);
+WeazelNews.sprite = 590;
+WeazelNews.color = 26;
+WeazelNews.name = "Weazel News";
+WeazelNews.shortRange = 1;
+
+const Fleecabank = new alt.PointBlip(150.07912, -1038.5934, 29.364136);
+Fleecabank.sprite = 605;
+Fleecabank.color = 2;
+Fleecabank.name = "Fleecabank";
+Fleecabank.shortRange = 1;
+
+const Zentralbank = new alt.PointBlip(236.33406, 217.04176, 106.28357);
+Zentralbank.sprite = 500;
+Zentralbank.color = 15;
+Zentralbank.name = "Nationalbank Los Santos";
+Zentralbank.shortRange = 1;
+
+const Fahrschule = new alt.PointBlip(222.36923, -1394.2682, 30.57727);
+Fahrschule.sprite = 595;
+Fahrschule.color = 38;
+Fahrschule.name = "Los Santos Driving School";
+Fahrschule.shortRange = 1;
+
+const Garage = new alt.PointBlip(215.52527, -803.9077, 30.762695);
+Garage.sprite = 524;
+Garage.color = 77;
+Garage.name = "Los Santos Garage";
+Garage.shortRange = 1;
+
+const Gefängnis = new alt.PointBlip(1855.9121, 2592, 44.950195);
+Gefängnis.sprite = 252;
+Gefängnis.color = 1;
+Gefängnis.name = "Los Santos Staatprison";
+Gefängnis.shortRange = 1;
+
+const Steinmine = new alt.PointBlip(2954.2021, 2781.7583, 39.9458);
+Steinmine.sprite = 485;
+Steinmine.color = 39;
+Steinmine.name = "Steinbruch";
+Steinmine.shortRange = 1;
+
+const Kohleberg = new alt.PointBlip(2706, 2826.079, 39.238037);
+Kohleberg.sprite = 486;
+Kohleberg.color = 39;
+Kohleberg.name = "Kohleberg";
+Kohleberg.shortRange = 1;
+
+const Mülldeponie = new alt.PointBlip(2354.4, 3133.9648, 47.47766);
+Mülldeponie.sprite = 318;
+Mülldeponie.color = 44;
+Mülldeponie.name = "Müllabfuhr";
+Mülldeponie.shortRange = 1;
+
+const Weizenfeld = new alt.PointBlip(2638.4702, 4688.5054, 34.183105);
+Weizenfeld.sprite = 85;
+Weizenfeld.color = 36;
+Weizenfeld.name = "Weizen";
+Weizenfeld.shortRange = 1;
+
+
+alt.on('gameEntityCreate', (entity) =>{
+    if(entity.hasStreamSyncedMeta("cityroleplay:teamcolor"))
+    {
+        let color = entity.getStreamSyncedMeta('cityroleplay:teamcolor');
+        let blip = native.getBlipFromEntity(entity.scriptID);
+        if(blip === 0) blip = native.addBlipForEntity(entity.scriptID);
+        native.setBlipColour(blip, isNaN(color) ? ß : color);
+        native.setBlipCategory(blip,7);
+        native.showHeadingIndicatorOnBlip(blip, true);
+    }
+})
+
+
+alt.on('gameEntityDestroy', (entity) => {
+    if(entity.hasStreamSyncedMeta('cityroleplay:teamcolor')){
+        let blip = native.getBlipFromEntity(entity.scriptID);
+        if(blip !== 0) native.removeBlip(blip);
+    }
+})
+
+alt.on('streamSyncedMetaChange', (entity, key, value, oldvalue) => {
+    if(key === 'cityroleplay:teamcolor'){
+        let color = entity.getStreamSyncedMeta("cityroleplay:teamcolor");
+        let blip = native.getBlipFromEntity(entity.scriptID);
+        if(blip === 0) blip = native.addBlipForEntity(entity.scriptID);
+        native.setBlipColour(blip, isNaN(color) ? 0 : color);
+        native.setBlipCategory(blip, 7);
+        native.showHeadingIndicatorOnBlip(blip, true); 
+    }
+})
